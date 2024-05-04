3a823415f93c843c65186944626fb5cc3740f987
esref <esrefsukurov7@gmail.com>
2024-05-04 15:09:20 +0400

    crud apis are done

---
+++ library-management/Controllers/BookController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using library_management.Models;
+using library_management.Data;
+using System.Data.Entity;
+
+namespace library_management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public BookController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> Get()
+        {
+            var books = _context.Books;
+            return Ok(books);
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public ActionResult<Book> Get(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<Book> Post([FromBody] Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(Get), new { id = book.Id }, book);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(book).State = (Microsoft.EntityFrameworkCore.EntityState)EntityState.Modified;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}
---
+++ library-management/Data/ApiContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using library_management.Models;
+
+namespace library_management.Data
+{
+    public class ApiContext : DbContext
+
+    {
+        public DbSet<Book> Books { get; set; }
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+
+    }
+}
